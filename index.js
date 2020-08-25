b8531f51e1d5663002444b71afc41124cbe49a8a
sean-slaughter <sthompson2822@gmail.com>
2020-08-25 15:12:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const output = [];
+    for(let i = 0; i < names.length; i++){
+        output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return output;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+}
